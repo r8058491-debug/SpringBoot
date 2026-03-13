ce044fa27c109c9efa972e52f32abd4dee40d663
r8058491-debug <r8058491@gmail.com>
2026-03-13 07:17:22 +0530

    update onetoone

--- onetoone/src/main/java/com/example/onetoone/dto/ProfileDTO.java
+++ onetoone/src/main/java/com/example/onetoone/dto/ProfileDTO.java
@@ -1,10 +1,12 @@
 package com.example.onetoone.dto;
 
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 public class ProfileDTO {
     @Id
-    @GeneratedValue
-    
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
